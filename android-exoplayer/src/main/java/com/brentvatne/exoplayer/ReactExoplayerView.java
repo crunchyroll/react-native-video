3f3520fab5b2266e560fca44c08d4ccd9cc548a8
Armands Malejevs <armands.malejevs@gmail.com>
2022-04-07 13:48:22 +0300

    Fix build issues

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -537,11 +537,11 @@ class ReactExoplayerView extends FrameLayout implements
                                 // DRM initialization must run on a different thread
                                 DrmSessionManager drmSessionManager = initializePlayerDrm(self);
                                 Log.w("Velocity", "DEBUG7");
-                                if (drmSessionManager == null && this.drmUUID != null) {
+                                if (drmSessionManager == null && self.drmUUID != null) {
                                     Log.w("Velocity", "DEBUG8");
                                     // Failed to intialize DRM session manager - cannot continue
                                     Log.e("ExoPlayer Exception", "Failed to initialize DRM Session Manager Framework!");
-                                    eventEmitter.error("Failed to initialize DRM Session Manager Framework!", ex, "3003");
+                                    eventEmitter.error("Failed to initialize DRM Session Manager Framework!", new Exception("DRM Session Manager Framework failure!"), "3003");
                                     return;
                                 }
 
