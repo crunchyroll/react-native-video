36ad93f0a42c497c5b0dec1f872ccd36d21c1161
Nick <nickfujita@gmail.com>
2022-05-27 23:06:24 +0900

    'logging'

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerLoadErrorHandlingPolicy.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerLoadErrorHandlingPolicy.java
@@ -18,7 +18,7 @@ public final class ReactExoplayerLoadErrorHandlingPolicy extends DefaultLoadErro
   @Override
   public long getRetryDelayMsFor(LoadErrorInfo loadErrorInfo) {
     Log.d("nicktest", loadErrorInfo.exception.getMessage());
-    Log.d("nicktest", loadErrorInfo.errorCount);
+    Log.d("nicktest", loadErrorInfo.errorCount.toString());
     if (
       loadErrorInfo.exception instanceof HttpDataSourceException &&
       (loadErrorInfo.exception.getMessage() == "Unable to connect" || loadErrorInfo.exception.getMessage() == "Software caused connection abort")
