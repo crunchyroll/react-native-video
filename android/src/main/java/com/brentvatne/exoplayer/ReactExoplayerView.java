b9d34586caf40e7526bef79b15adc117c201b608
Armands Malejevs <armands.malejevs@gmail.com>
2023-03-07 10:46:15 +0200

    Fix language support

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -2246,6 +2246,8 @@ public class ReactExoplayerView extends FrameLayout implements
     }
 
     public void setUiLanguage(String language) {
+        Log.w("RNV_CSAI", "setUiLanguage:");
+        Log.w("RNV_CSAI", language);
         this.uiLanguage = language;
         if (imaSettings != null) {
             imaSettings.setLanguage(language);
