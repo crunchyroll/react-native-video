8930bad6f18ef96a149531dfcd56fb10e2572d67
Armands Malejevs <armands.malejevs@gmail.com>
2023-03-07 22:00:54 +0200

    debug

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -536,7 +536,7 @@ public class ReactExoplayerView extends FrameLayout implements
         reLayout(exoPlayerView);
     }
 
-    public void handleAdStarted(AdEvent event) {
+    public void handleCheckTruex(AdEvent event) {
         if (activeAd == null) {
             Log.w("RNV_CSAI", "No Active ad to determine TrueX");
             return;
@@ -580,7 +580,7 @@ public class ReactExoplayerView extends FrameLayout implements
                 reLayout(exoPlayerView);
                 eventEmitter.adEvent("STARTED", payload);
                 Log.w("RNV_CSAI", "Ad started");
-                handleAdStarted(event);
+                handleCheckTruex(event);
                 break;
             case CONTENT_RESUME_REQUESTED:
                 activeAd = null;
@@ -588,6 +588,15 @@ public class ReactExoplayerView extends FrameLayout implements
                 break;
             case TAPPED:
                 eventEmitter.videoClickEvent();
+                break;
+            case LOADED:
+                Log.w("RNV_CSAI", "Ads loaded");
+                handleCheckTruex(event);
+                break;
+            case AD_BREAK_ENDED:
+                Log.w("RNV_CSAI", "Ads AD_BREAK_ENDED");
+                handleCheckTruex(event);
+                break;
         } 
     }
 
