6f4be81beb18defef856553bad46e09cf6ef9d77
Armands Malejevs <armands.malejevs@gmail.com>
2023-03-10 10:20:27 +0200

    Change layout

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -349,7 +349,7 @@ public class ReactExoplayerView extends FrameLayout implements
         // Add Exoplayer view
         addView(exoPlayerView, 0, layoutParams);
         // Add TrueX Layout
-        exoPlayerView.addView(truexViewGroup, -1, layoutParams);
+        addView(truexViewGroup, -1, layoutParams);
 
         imaSettings = ImaSdkFactory.getInstance().createImaSdkSettings();
         imaSettings.setLanguage(uiLanguage);
@@ -469,7 +469,7 @@ public class ReactExoplayerView extends FrameLayout implements
             Log.w("RNV_CSAI", "NO PLAYER CANNOT WILL NOT LOOK FOR TRUEX");
             return;
         }
-
+        Log.w("RNV_CSAI", "Pausing playback for TrueX");
         // Pause the stream and display a true[X] engagement
         pausePlayback();
         Long position = player.getCurrentPosition();
@@ -478,8 +478,10 @@ public class ReactExoplayerView extends FrameLayout implements
         //displayMode = DisplayMode.INTERACTIVE_AD;
 
         // Start the true[X] engagement
+        Log.w("RNV_CSAI", "Starting TrueXAdManager");
         ViewGroup viewGroup = (ViewGroup)truexViewGroup;
         truexAdManager = new TruexAdManager(getContext(), this);
+        Log.w("RNV_CSAI", "Starting TrueX Ad");
         truexAdManager.startAd(viewGroup, vastUrl);
         reLayout(exoPlayerView);
     }
@@ -542,8 +544,7 @@ public class ReactExoplayerView extends FrameLayout implements
                 Log.w("RNV_CSAI", "Ads AD_BREAK_ENDED");
                 handleCheckTruex(event);
                 break;
-        } 
-        Log.w("RNV_CSAI", "====================== AD EVENT END ======================");
+        }
     }
 
     @Override
