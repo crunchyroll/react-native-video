33e0b925ef21de63444d1e434e549759bf69adf4
Armands Malejevs <armands.malejevs@gmail.com>
2023-03-13 16:06:12 +0200

    Layout changes

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -533,8 +533,8 @@ public class ReactExoplayerView extends FrameLayout implements
 
     public void reLayoutRoot() {
         Log.w("RNV_CSAI", "Forcing layout update - truex!");
-        this.invalidate();
-        exoPlayerView.invalidate();
+        //this.invalidate();
+        //exoPlayerView.invalidate();
         this.reLayout(exoPlayerView);
     }
 
@@ -550,9 +550,8 @@ public class ReactExoplayerView extends FrameLayout implements
         displayInteractiveAd(vastUrl);
         } else {
             Log.w("RNV_CSAI", "Ad System is not TrueX");
-            
         }
-
+        reLayout(exoPlayerView);
     }
 
     @Override
@@ -578,8 +577,6 @@ public class ReactExoplayerView extends FrameLayout implements
         AdEvent.AdEventType eventType = event.getType();
         switch(eventType) {
             case STARTED:
-                reLayout(exoPlayerView);
-
                 eventEmitter.adEvent("STARTED", payload);
                 Log.w("RNV_CSAI", "Ad started");
                 handleCheckTruex(event);
