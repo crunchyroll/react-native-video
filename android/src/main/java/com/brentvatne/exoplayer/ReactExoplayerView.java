36dd4b90a0a7af5e54ed43aef3f9a993146d5de4
Armands Malejevs <armands.malejevs@gmail.com>
2023-03-13 14:39:57 +0200

    Invalidate view on truex ad

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -519,6 +519,7 @@ public class ReactExoplayerView extends FrameLayout implements
 
     public void reLayoutRoot() {
         Log.w("RNV_CSAI", "Forcing layout update - truex!");
+        exoPlayerView.invalidate();
         this.reLayout(exoPlayerView);
     }
 
