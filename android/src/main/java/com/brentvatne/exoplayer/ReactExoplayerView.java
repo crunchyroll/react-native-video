b04fc278446bf3c4406f924c9e4d27d45ebac0d0
Armands Malejevs <armands.malejevs@gmail.com>
2023-03-13 15:54:25 +0200

    Layout changes

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -342,17 +342,9 @@ public class ReactExoplayerView extends FrameLayout implements
         exoPlayerView = new ExoPlayerView(getContext());
         exoPlayerView.setLayoutParams(layoutParams);
 
-        // TrueX Layout
-        truexViewGroup = new FrameLayout(getContext());
-        truexViewGroup.setLayoutParams(layoutParams);
-        
-        // Add TrueX Layout
-        exoPlayerView.addView(truexViewGroup, -1, layoutParams);
-
         // Add Exoplayer view
         addView(exoPlayerView, 0, layoutParams);
         
-
         mainHandler = new Handler();
     }
 
@@ -510,11 +502,11 @@ public class ReactExoplayerView extends FrameLayout implements
 
         // Start the true[X] engagement
         Log.w("RNV_CSAI", "Starting TrueXAdManager");
-        ViewGroup viewGroup = (ViewGroup)truexViewGroup;
+        ViewGroup viewGroup = (ViewGroup)exoplayerView.getTruexViewGroup();
         truexAdManager = new TruexAdManager(getContext(), this);
         truexAdManager.setReactExoPlayerView(this);
         Log.w("RNV_CSAI", "Starting TrueX Ad");
-        truexAdManager.startAd((ViewGroup)this, vastUrl);
+        truexAdManager.startAd(viewGroup, vastUrl);
     }
 
     public void reLayoutRoot() {
