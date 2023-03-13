850c839086c6940fdffe2adcdc347d2c659b54a9
Armands Malejevs <armands.malejevs@gmail.com>
2023-03-13 16:35:58 +0200

    TrueX events

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -457,6 +457,8 @@ public class ReactExoplayerView extends FrameLayout implements
         if (player == null) {
             return;
         }
+        WritableMap payload = Arguments.createMap();
+        eventEmitter.adEvent("ENDED_TRUEX", payload);
         this.googleAdsLoader = this.adsLoader.getAdsLoader();
         if (this.googleAdsLoader != null) {
            // this.adsManager.discardAdBreak();
@@ -473,6 +475,8 @@ public class ReactExoplayerView extends FrameLayout implements
         if (this.player == null) {
             return;
         }
+        WritableMap payload = Arguments.createMap();
+        eventEmitter.adEvent("ENDED_TRUEX", payload);
         this.adsLoader.skipAd();
         this.startPlayback();
         
@@ -492,6 +496,10 @@ public class ReactExoplayerView extends FrameLayout implements
             Log.w("RNV_CSAI", "NO PLAYER CANNOT WILL NOT LOOK FOR TRUEX");
             return;
         }
+
+        WritableMap payload = Arguments.createMap();
+        eventEmitter.adEvent("STARTED_TRUEX", payload);
+
         Log.w("RNV_CSAI", "Pausing playback for TrueX");
         // Pause the stream and display a true[X] engagement
         pausePlayback();
