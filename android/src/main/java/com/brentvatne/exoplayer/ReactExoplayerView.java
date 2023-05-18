993081d924bb549bb65fbe58f5a41b11ea568483
Armands Malejevs <armands.malejevs@gmail.com>
2023-05-18 15:31:42 +0300

    Update import

--- android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -123,7 +123,7 @@ import com.mux.stats.sdk.core.model.CustomData;
 
 import com.mux.stats.sdk.muxstats.MuxErrorException;
 import com.mux.stats.sdk.muxstats.MuxStatsExoPlayer;
-import static com.mux.stats.sdk.muxstats.PlayerKt.monitorWithMuxData;
+import static com.mux.stats.sdk.muxstats.ExoPlayerExtensionsKt.monitorWithMuxData;
 // End Mux
 
 import java.io.IOException;
